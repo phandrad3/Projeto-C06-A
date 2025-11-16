771e345f76aaa63feb69889fd5885432e9dc42e8
Danilo <danilo.henrique@gec.inatel.br>
2025-11-16 17:49:09 -0300

    Delete LojaBumi directory

--- LojaBumi/src/main/java/com/example/lojabumi/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.lojabumi;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- LojaBumi/src/main/java/com/example/lojabumi/produtos/tipo/Eletronico.java
+++
@@ -1,9 +0,0 @@
-package com.example.lojabumi.produtos.tipo;
-
-import com.example.lojabumi.produtos.Produto;
-
-public class Eletronico extends Produto {
-    public Eletronico(String nome, double preco) {
-        super(nome, preco);
-    }
-}
